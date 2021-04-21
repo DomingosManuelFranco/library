2bd11570b7efe417515a0a5158fd31a625bb7cdc
DomingosManuelFranco <manuelfranco070@gmail.com>
2021-04-21 09:00:29 +0100

    the hmtl structure has been created

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let myLibrary = [
+    {
+        title = "Come to Be my Follower",
+        author = "Jeff Jackson",
+        pages = 150,
+        info = function () {
+            return `${title} by ${author}, ${pages}, ${read()}.`;
+        }
+    },
+
+    {
+        title = "Imitate their Faith",
+        author = "Mark Sanderson",
+        pages = 250,
+        info = function () {
+            return `${title} by ${author}, ${pages}, ${read()}.`;
+        }
+    },
+
+    {
+        title = "My Book of Bible Stories",
+        author = "WatchTower Inc",
+        pages = 430,
+        info = function () {
+            return `${title} by ${author}, ${pages}, ${read()}.`;
+        }
+    }
+];
