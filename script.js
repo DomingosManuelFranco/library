ada7f01946c5b92f813726095da2706998ff075a
DomingosManuelFranco <manuelfranco070@gmail.com>
2021-04-21 15:48:48 +0100

    small changes

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 
 let myLibrary = [
     {
-        title = "Come to Be my Follower",
-        author = "Jeff Jackson",
-        pages = 150,
-        info = function () {
-            return `${title} by ${author}, ${pages}, ${read()}.`;
+        title: "Come to Be my Follower",
+        author: "Jeff Jackson",
+        pages: 150,
+        info: function () {
+             return `${this.title} by ${this.author}, ${this.pages}, ${this.read()}.`;
         }
     },
 
     {
-        title = "Imitate their Faith",
-        author = "Mark Sanderson",
-        pages = 250,
-        info = function () {
-            return `${title} by ${author}, ${pages}, ${read()}.`;
+        title: "Imitate their Faith",
+        author: "Mark Sanderson",
+        pages: 250,
+        info: function () {
+            return `${this.title} by ${this.author}, ${this.pages}, ${this.read()}.`;
         }
     },
 
     {
-        title = "My Book of Bible Stories",
-        author = "WatchTower Inc",
-        pages = 430,
-        info = function () {
-            return `${title} by ${author}, ${pages}, ${read()}.`;
+        title: "My Book of Bible Stories",
+        author: "WatchTower Inc",
+        pages: 430,
+        info: function () {
+            return `${this.title} by ${this.author}, ${this.pages}.`;
         }
     }
-];
+]
+console.log(myLibrary);
+
+function addBookToLibrary(params) {
+    
+}
